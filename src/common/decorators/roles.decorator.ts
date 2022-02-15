2605d85eb7285d538f198f45f3ef94960840f1ed
Aleksandr Aladin <aleksandr_aladin@outlook.com>
2022-02-15 17:02:53 +0300

    feat: add roles decorator

---
+++ src/common/decorators/roles.decorator.ts
@@ -0,0 +1,7 @@
+import { SetMetadata } from '@nestjs/common';
+
+import { UserRole } from '../../user/enums/user-role.enum';
+
+export const ROLES_KEY = 'roles';
+
+export const Roles = (...roles: UserRole[]) => SetMetadata(ROLES_KEY, roles);

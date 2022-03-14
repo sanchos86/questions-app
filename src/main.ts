8b0d38ad20c71d6562d8a94ea195c369c864307f
Aleksandr Aladin <aleksandr_aladin@outlook.com>
2022-03-14 20:01:24 +0300

    fix: set port through env variable

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,6 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.use(cookieParser());
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();

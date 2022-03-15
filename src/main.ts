a5d1360b20ae8ba101dac62e3570ebcf960adda7
Aleksandr Aladin <aleksandr_aladin@outlook.com>
2022-03-15 09:56:12 +0300

    fix: fix swagger url  bug

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { ClassSerializerInterceptor } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule, DocumentBuilder, SwaggerDocumentOptions } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 import { corsOptions } from './config/cors';
@@ -9,15 +9,19 @@ import { corsOptions } from './config/cors';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: corsOptions });
 
-  if (process.env.NODE_ENV !== 'production') {
-    const config = new DocumentBuilder().setTitle('Questions app').build();
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('swagger', app, document);
-  }
-
   app.setGlobalPrefix('api');
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.use(cookieParser());
+
+  if (process.env.NODE_ENV !== 'production') {
+    const options: SwaggerDocumentOptions = {
+      ignoreGlobalPrefix: false,
+    };
+    const config = new DocumentBuilder().setTitle('Questions app').build();
+    const document = SwaggerModule.createDocument(app, config, options);
+    SwaggerModule.setup('swagger', app, document);
+  }
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
